68c741b07e3093bebea2c049180b686461030042
vladandrei51 <vladandrei51@gmail.com>
2018-02-08 12:55:40 +0200

    cleared up build.gradle, changed min sdk

--- app/src/main/java/com/example/vlada/licenta/Views/ExerciseView.java
+++ app/src/main/java/com/example/vlada/licenta/Views/ExerciseView.java
@@ -2,9 +2,7 @@ package com.example.vlada.licenta.Views;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.text.method.ScrollingMovementMethod;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.widget.TextView;
 
 import com.example.vlada.licenta.Domain.Exercise;
@@ -20,7 +18,7 @@ import io.realm.Realm;
 public class ExerciseView extends BaseActivity {
 
     private static final int SWIPE_THRESHOLD = 100;
-    TextView textView;
+    TextView exerciseName;
     private Realm realm;
     private Exercise exercise;
     private float _downX;
@@ -30,8 +28,7 @@ public class ExerciseView extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
 
-        textView = findViewById(R.id.textView);
-        textView.setMovementMethod(new ScrollingMovementMethod());
+        exerciseName = findViewById(R.id.textView);
         this.realm = Realm.getDefaultInstance();
         if (getIntent().getExtras() != null) {
             exercise = realm.where(Exercise.class).equalTo("name", getIntent().getExtras().getString("exercise_name")).findFirst();
@@ -47,7 +44,7 @@ public class ExerciseView extends BaseActivity {
 
     private void populatePage() {
         setTitle(exercise.getName() + " (" + exercise.getMusclegroup() + ")");
-        textView.setText(exercise.getDescription());
+        exerciseName.setText(exercise.getName());
     }
 
     //    For enabling back button on top of the activity
@@ -59,21 +56,6 @@ public class ExerciseView extends BaseActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent evt) {
-        switch (evt.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                _downX = evt.getX();
-            case MotionEvent.ACTION_UP:
-                float deltaX = evt.getX() - _downX;
-
-                if (Math.abs(deltaX) > SWIPE_THRESHOLD && deltaX < 0)
-                    finish();
-        }
-
-        return true;
     }
 
 
