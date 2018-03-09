094279d30398c287c95e28c6570803bcc1b92fa2
vladandrei51 <vladandrei51@gmail.com>
2018-03-09 16:15:38 +0200

    Attempt edit button TODO: see details (+notes), edit lift

--- app/src/main/java/com/example/vlada/licenta/Views/ExerciseLiftFragment.java
+++ app/src/main/java/com/example/vlada/licenta/Views/ExerciseLiftFragment.java
@@ -1,5 +1,8 @@
 package com.example.vlada.licenta.Views;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -10,7 +13,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.example.vlada.licenta.Base.BaseFragment;
 import com.example.vlada.licenta.Domain.Exercise;
@@ -20,6 +28,7 @@ import com.example.vlada.licenta.Utils.AdapterLiftRecycler;
 import com.example.vlada.licenta.Utils.Utils;
 
 import java.util.Comparator;
+import java.util.Date;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
@@ -33,9 +42,9 @@ public class ExerciseLiftFragment extends BaseFragment {
 
     Exercise mExercise;
     FloatingActionButton mAddBT;
+    Lift mClickedLift;
 
     String mExerciseName;
-    DialogFragment mDialog;
     private RealmResults<Lift> mResults;
     private RecyclerView mRecyclerView;
     private AdapterLiftRecycler mAdapter;
@@ -54,70 +63,81 @@ public class ExerciseLiftFragment extends BaseFragment {
 
     public void deleteLift(Lift clickedLift) {
         if (clickedLift != null) {
-            Realm realm;
-            realm = Realm.getDefaultInstance();
-            realm.executeTransaction(realm1 -> {
-                RealmResults<Lift> result = realm1.where(Lift.class)
-                        .equalTo("notes", clickedLift.getNotes())
-                        .equalTo("reps", clickedLift.getReps())
-                        .equalTo("weight", clickedLift.getWeight())
-                        .equalTo("date_ms", clickedLift.getDate_ms())
-                        .equalTo("exercise_name", clickedLift.getExercise_name())
-                        .findAll();
-                result.deleteAllFromRealm();
-            });
-            realm.close();
-            updateList();
-            Utils.displayToast(getContext(), "Successfully deleted");
+            deleteLiftFromRealm(clickedLift);
         }
     }
+
+    private void deleteLiftFromRealm(Lift lift) {
+        Realm realm;
+        realm = Realm.getDefaultInstance();
+        realm.executeTransaction(realm1 -> {
+            RealmResults<Lift> result = realm1.where(Lift.class)
+                    .equalTo("notes", lift.getNotes())
+                    .equalTo("reps", lift.getReps())
+                    .equalTo("weight", lift.getWeight())
+                    .equalTo("date_ms", lift.getDate_ms())
+                    .equalTo("exercise_name", lift.getExercise_name())
+                    .findAll();
+            result.deleteAllFromRealm();
+        });
+        realm.close();
+    }
+
+    public void editLift(Lift clickedLift) {
+        if (clickedLift != null) {
+            mClickedLift = clickedLift;
+            showLiftDialog(true);
+        }
+    }
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         View rootView = inflater.inflate(R.layout.fragment_exercise_lift, container, false);
 
         if (getArguments() != null)
             mExerciseName = getArguments().getString("exercise_name");
-        mAddBT = rootView.findViewById(R.id.fab);
-        mRecyclerView = rootView.findViewById(R.id.historyLV);
-
-
         mExercise = (Exercise) mRealmHelper.getRealmObject(Exercise.class, "name", mExerciseName);
+
+        mAddBT = rootView.findViewById(R.id.fab);
+        mAddBT.setOnClickListener(v -> showLiftDialog(false));
+
+        mRecyclerView = rootView.findViewById(R.id.historyLV);
         this.mResults = mRealmHelper.findAllFilteredSorted(Lift.class, "exercise_name", mExerciseName, "setDate", Sort.DESCENDING);
         populateList();
-        addButtonClickListener();
         return rootView;
     }
 
-    public void showAddLiftDialog() {
-        mDialog = new AddLiftDialog();
-        mDialog.setTargetFragment(this, 0);
+    public void showLiftDialog(boolean edit) {
+        DialogFragment liftDialog = LiftDialog.newInstance(edit);
+        liftDialog.setTargetFragment(this, 0);
         if (getFragmentManager() != null) {
-            mDialog.show(getFragmentManager(), "addLiftDialog");
+            liftDialog.show(getFragmentManager(), "dialog");
         } else {
             Utils.displayToast(getContext(), "Error");
         }
     }
 
 
-    void addButtonClickListener() {
-        mAddBT.setOnClickListener(v -> showAddLiftDialog());
+    private void updateLiftFromRealm(Lift originalLift, Lift newLift) {
+        deleteLiftFromRealm(originalLift);
+        mRealmHelper.insert(newLift);
+        updateRVList();
+        Utils.displayToast(getContext(), "Successfully updated");
 
     }
 
     public void insertLiftFromDialog(Lift lift) {
-        if (lift != null) {
+        if (lift != null && mExercise != null) {
             float highest1RMPreAdd = mResults
                     .stream()
                     .map(Utils::getEstimated1RM)
                     .max(Comparator.naturalOrder())
                     .orElse(0f);
-            if (mExercise.getId() > 0) lift.setExercise(mExercise);
-
+            lift.setExercise(mExercise);
             mRealmHelper.insert(lift);
-            updateList();
+            updateRVList();
             Utils.displayToast(getContext(), "Successfully added");
 
             if (highest1RMPreAdd < Utils.getEstimated1RM(lift)) {
@@ -141,9 +161,13 @@ public class ExerciseLiftFragment extends BaseFragment {
     }
 
 
-    public void updateList() {
+    public void updateRVList() {
         mAdapter = new AdapterLiftRecycler(mResults, getContext(), new ItemsListener(), this);
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+    public Lift getClickedLift() {
+        return mClickedLift;
     }
 
     @Override
@@ -151,6 +175,100 @@ public class ExerciseLiftFragment extends BaseFragment {
         super.onDestroyView();
     }
 
+    public static class LiftDialog extends DialogFragment {
+        Button mCancelBT;
+        Button mConfirmButton;
+        EditText mWeightET;
+        EditText mRepsET;
+        CheckBox mNotesCB;
+        EditText mNotesET;
+        Lift mLift2Add;
+        Lift mLift2Edit;
+        boolean mEditDialog;
+        ExerciseLiftFragment mExerciseLiftFragment;
+
+        public static LiftDialog newInstance(boolean value) {
+            LiftDialog f = new LiftDialog();
+
+            Bundle args = new Bundle();
+            args.putBoolean("edit", value);
+            f.setArguments(args);
+            return f;
+        }
+
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            assert getActivity() != null;
+            View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_lift, new LinearLayout(getActivity()), false);
+            if (getArguments() != null)
+                mEditDialog = getArguments().getBoolean("edit");
+
+            mExerciseLiftFragment = (ExerciseLiftFragment) getTargetFragment();
+
+            mCancelBT = view.findViewById(R.id.cancelDialogBT);
+            mConfirmButton = view.findViewById(R.id.confirmDialogButton);
+            mConfirmButton.setText(mEditDialog ? "Edit" : "Add");
+            mWeightET = view.findViewById(R.id.weight);
+            mRepsET = view.findViewById(R.id.reps);
+            mNotesCB = view.findViewById(R.id.notesCB);
+            mNotesET = view.findViewById(R.id.notes);
+            mLift2Add = new Lift();
+            mLift2Edit = (mExerciseLiftFragment.getClickedLift());
+
+            Dialog builder = new Dialog(getActivity());
+            if (builder.getWindow() != null) {
+                builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                builder.getWindow().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#E0E0E0")));
+                builder.setContentView(view);
+            }
+            if (mLift2Edit != null && mEditDialog) {
+                mNotesET.setText(mLift2Edit.getNotes());
+                mRepsET.setText(String.valueOf(mLift2Edit.getReps()));
+                mWeightET.setText(String.valueOf(mLift2Edit.getWeight()));
+                mNotesCB.setChecked(mLift2Edit.getNotes().length() > 0);
+                mNotesET.setVisibility(mLift2Edit.getNotes().length() > 0 ? View.VISIBLE : View.GONE);
+            }
+            listeners();
+            return builder;
+        }
+
+        private void listeners() {
+            mConfirmButton.setOnClickListener(v -> {
+                if (!mEditDialog) {
+                    mLift2Add.setNotes(mNotesET.getText().toString().length() > 0 && mNotesCB.isChecked() ? mNotesET.getText().toString() : "");
+                    mLift2Add.setReps(mRepsET.getText().toString().length() > 0 ? Integer.parseInt(mRepsET.getText().toString()) : 0);
+                    mLift2Add.setWeight(mWeightET.getText().toString().length() > 0 ? Integer.parseInt(mWeightET.getText().toString()) : 0);
+                    mLift2Add.setSetDate(new Date());
+                    dismissDialog();
+                    mExerciseLiftFragment.insertLiftFromDialog(mLift2Add);
+                } else {
+                    Lift newLift = new Lift();
+                    newLift.setSetDate(mLift2Edit.getSetDate());
+                    newLift.setNotes(mNotesET.getText().toString().length() > 0 && mNotesCB.isChecked() ? mNotesET.getText().toString() : "");
+                    newLift.setWeight(Integer.parseInt(mWeightET.getText() + ""));
+                    newLift.setReps(Integer.parseInt(mRepsET.getText() + ""));
+                    dismissDialog();
+                    mExerciseLiftFragment.updateLiftFromRealm(mLift2Edit, newLift);
+                }
+            });
+            mCancelBT.setOnClickListener(v -> dismissDialog());
+            mNotesCB.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                if (isChecked) {
+                    mNotesET.setVisibility(View.VISIBLE);
+                } else {
+                    mNotesET.setVisibility(View.GONE);
+                }
+
+            });
+
+        }
+
+        private void dismissDialog() {
+            LiftDialog.this.getDialog().cancel();
+        }
+    }
 
     class ItemsListener implements AdapterView.OnClickListener {
         @Override
@@ -159,6 +277,7 @@ public class ExerciseLiftFragment extends BaseFragment {
 //            Utils.displayToast(getContext(), "clicked");
         }
     }
+
 
 }
 
