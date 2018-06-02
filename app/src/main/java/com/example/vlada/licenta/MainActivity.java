4ff6ca2e8933613099ca4cf64beab81ba8eff790
vladandrei51 <vladandrei51@gmail.com>
2018-06-02 17:42:31 +0300

    Added home page + menu FOB

--- app/src/main/java/com/example/vlada/licenta/MainActivity.java
+++ app/src/main/java/com/example/vlada/licenta/MainActivity.java
@@ -4,16 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.example.vlada.licenta.Views.ExerciseListView;
+import com.example.vlada.licenta.Views.HomeActivity;
+
+import javax.annotation.Nullable;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Realm.init(this);
@@ -23,9 +24,7 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         Realm.setDefaultConfiguration(config);
 
-//        Utils.addPlaceHolderLifts();
-
-        Intent intent = new Intent(MainActivity.this, ExerciseListView.class);
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
         startActivity(intent);
         finish();
     }
---
+++ app/src/main/java/com/example/vlada/licenta/Views/HomeActivity.java
@@ -0,0 +1,117 @@
+package com.example.vlada.licenta.Views;
+
+import android.animation.Animator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.vlada.licenta.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeActivity extends AppCompatActivity {
+    private static boolean isFabOpen;
+    FloatingActionButton mMainFab;
+    FloatingActionButton mLiftingFab;
+    FloatingActionButton mCardioFab;
+    View bgFabMenu;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setContentView(R.layout.layout_main);
+        mMainFab = findViewById(R.id.main_fab);
+        mLiftingFab = findViewById(R.id.lifting_fab);
+        mCardioFab = findViewById(R.id.cardio_fab);
+        bgFabMenu = findViewById(R.id.bg_fab_menu);
+
+        mMainFab.setOnClickListener(v -> {
+            if (!isFabOpen) {
+                ShowFabMenu();
+            } else
+                CloseFabMenu();
+        });
+
+        mLiftingFab.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, ExerciseListView.class);
+            startActivity(intent);
+
+        });
+
+        bgFabMenu.setOnClickListener(view -> {
+            CloseFabMenu();
+        });
+
+        super.onCreate(savedInstanceState);
+    }
+
+    private void ShowFabMenu() {
+        isFabOpen = true;
+        mLiftingFab.setVisibility(View.VISIBLE);
+        mCardioFab.setVisibility(View.VISIBLE);
+        bgFabMenu.setVisibility(View.VISIBLE);
+
+        mMainFab.animate().rotation(360f);
+        bgFabMenu.animate().alpha(1f);
+        mLiftingFab.animate()
+                .translationY(-(mMainFab.getTranslationY() + mMainFab.getHeight() * 1.1f))
+                .rotation(0f);
+        mCardioFab.animate()
+                .translationY(-(mMainFab.getTranslationY() + mMainFab.getHeight() * 2))
+                .rotation(0f);
+    }
+
+    private void CloseFabMenu() {
+        isFabOpen = false;
+
+        mMainFab.animate().rotation(0f);
+        bgFabMenu.animate().alpha(0f);
+        mCardioFab.animate()
+                .translationY(0f)
+                .rotation(90f);
+        mLiftingFab.animate()
+                .translationY(0f)
+                .rotation(90f).setListener(new FabAnimatorListener(bgFabMenu, mLiftingFab, mCardioFab));
+    }
+
+
+    private class FabAnimatorListener implements Animator.AnimatorListener {
+        List<View> mViewsToHide;
+
+        FabAnimatorListener(View bgFabMenu, FloatingActionButton mLiftingFab, FloatingActionButton mCardioFab) {
+            mViewsToHide = new ArrayList<>();
+            mViewsToHide.clear();
+            mViewsToHide.add(bgFabMenu);
+            mViewsToHide.add(mLiftingFab);
+            mViewsToHide.add(mCardioFab);
+        }
+
+        @Override
+        public void onAnimationStart(Animator animator) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animator animator) {
+            if (!isFabOpen) {
+                for (View object : mViewsToHide) {
+                    object.setVisibility(View.GONE);
+                }
+            }
+        }
+
+        @Override
+        public void onAnimationCancel(Animator animator) {
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animator animator) {
+
+        }
+    }
+}
